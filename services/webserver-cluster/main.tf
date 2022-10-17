d44b8566464be9ac63661cf4f1a1bc8c93f00b9d
Peanut S <dumbking+github@gmail.com>
2022-10-17 21:05:12 +0700

    added rolling update to asg

--- services/webserver-cluster/main.tf
+++ services/webserver-cluster/main.tf
@@ -24,6 +24,7 @@ resource "aws_launch_configuration" "example" {
 }
 
 resource "aws_autoscaling_group" "example" {
+  name = "${var.cluster_name}-asg"
   launch_configuration = aws_launch_configuration.example.name
   vpc_zone_identifier = data.aws_subnets.default.ids
   target_group_arns = [aws_lb_target_group.asg.arn]
